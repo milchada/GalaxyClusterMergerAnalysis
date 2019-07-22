501cf7a3add8f0cbedffbf106c0428a529b7ed11
milchada <uchadaya@gmail.com>
2019-07-22 15:43:55 -0400

    cleanup compare.py

--- gcmerge/compare.py
+++ gcmerge/compare.py
@@ -11,9 +11,15 @@ import matplotlib.pylab as plt
 from read import open_fits, calibrate, init
 from align_BCGs import align_bcgs
 
-obsdata, errorsq, x, y, xp = init(obsfile, errfile)   
+data, errorsq, x, y, xp = init(obsfile, errfile)   
 
-def shift_compare(image, offset, angle, plot=False, time=None):
+def shift_image(image, offset):
+	xstep, ystep, startshift = offset
+	imagecut = np.roll(np.roll(image, xstep-startshift, axis=0), ystep-startshift, axis=1)
+	imagecut[np.isnan(imagecut)] = 0 #otherwise rotation gets fucked
+	return imagecut
+
+def shift_compare(image, offset=(0,0,0), plot=False, time=None):
 	shiftimage = shift_image(image, offset)
 	masked = np.ma.masked_equal(shiftimage, 0)
 	diff = (shiftimage - data)**2 / errorsq
@@ -31,42 +37,28 @@ def match(simfiles, potfiles, filenum, xraypeak, peak_threshold = 0.7, pixels_to
 	file = simfiles[filenum]
 	potfile = potfiles[filenum]
 
-	sdata, sheader = open_fits(file,0)
-	sx = calibrate(sdata.shape[1],sheader,axis=1)
-	sy = calibrate(sdata.shape[1],sheader,axis=2)
-	sx -= sx.mean()
-	sy -= sy.mean()
-	print("Snap %d read in" % filenum)
+	rot_image = align_bcgs(potfile, file)
+	binned_data = rot_image
+	binned_data[binned_data == 0] = np.nan
 
-	#interpolate
-	f = interp2d(sx, sy, sdata)
-	binned_data = f(x,y)
-	print("Data binned")
-
-	#align x-ray peaks
-	offset, rot_angle = offset_tilt(potfile)
-	coords -= bcg_peak 
-	coords *= -1 #how much to shift by
-	print("Xray peaks aligned")
-	
 	#optimise rescaling within circle 
 	l = np.nansum(data*binned_data/errorsq)/np.nansum(binned_data**2 / errorsq)
-	binned_data *= l
+	rot_image *= l
 	
 	if filenum > filemin: 
 		chisqs_around_peak = np.zeros([pixels_to_shift, pixels_to_shift])
 		for xstep in range(pixels_to_shift):
 			for ystep in range(pixels_to_shift):
-				chisqs_around_peak[xstep,ystep] = shift_rotate_compare(data, binned_data,offset=(xstep, ystep, pixels_to_shift/2))
+				chisqs_around_peak[xstep,ystep] = shift_compare(rot_image,offset=(xstep, ystep, pixels_to_shift/2))
 				print("xstep = %d, ystep = %d done!" % (xstep, ystep))
 
 		minx, miny = np.argmin(chisqs_around_peak)[0]
-		chisqs[filenum] = best_rotation(data, rerolled_data, (minx, miny, pixels_to_shift//2))
+		chisqs[filenum] = shift_compare(rot_image, (minx, miny, pixels_to_shift//2))
 		print("done!")
 
 	else:
 		print("halos not merged yet. done!")
-		chisqs[filenum] = shift_rotate_compare(data, binned_data, (0,0,0))
+		chisqs[filenum] = shift_compare(rot_image)
 
 	np.save('chisq%s' % suffix, chisqs)
 
